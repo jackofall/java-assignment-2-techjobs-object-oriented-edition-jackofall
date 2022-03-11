7f35041228214bf80bfeab652c295c0679266b45
jackofall <abbasaazhar@gmail.com>
2022-03-10 19:16:33 -0600

    update

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -6,6 +6,7 @@ import org.junit.runners.JUnit4;
 import org.launchcode.techjobs.oo.Job;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 /**
  * Created by LaunchCode
@@ -15,11 +16,14 @@ public class JobTest {
     @Test
     public void testSettingJobId() {
         Job myJob = new Job();
-        new Job();
+        Job myNewJob= new Job() ;
  assertEquals(1, myJob.getId());
+ assertEquals(2, myNewJob.getId());
+ assertFalse(myJob.getId()==myNewJob.getId());
     }
 
-
+    public JobTest() {
+    }
 }
 
 
